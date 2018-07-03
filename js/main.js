ca29f49ded572d556f41c088bc9d6fae35c368e7
Michael Morgan <michael1986morgan@gmail.com>
2018-07-03 19:16:58 -0400

    added mobile responsiveness

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#home-heading').fadeIn(2000);
+});
+
+$(section).onload(function() {
+  $('h2').fadeIn(1000);
+});
--- js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $(window).on('load', function() {
-   $("#home-heading").fadeIn(2000);
-    $("hr").fadeIn(3000);
-  });
-});
