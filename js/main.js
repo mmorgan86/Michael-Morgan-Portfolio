fd2c9321627b4870eb8a6f0eeff3050487f31323
Michael Morgan <michael1986morgan@gmail.com>
2018-07-04 15:43:07 -0400

    added smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// HEADER TEXT FADE IN ON PAGE LOAD
 $(document).ready(function() {
   $('#home-heading').fadeIn(2000);
+});
+
+// SMOOTH SCROLL
+$(document).on('click', 'a[href^="#"]', function(e) {
+  e.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
 });
