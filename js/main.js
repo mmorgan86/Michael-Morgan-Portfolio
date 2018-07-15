1905fd1f8c3f49c9e871a13a2c29f459c665a3d6
Michael Morgan <michael1986morgan@gmail.com>
2018-07-15 19:19:59 -0400

    changed contact text

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 // HEADER TEXT FADE IN ON PAGE LOAD
 $(document).ready(function() {
-  $('#home-heading').fadeIn(2000);
+    $('#home-heading').fadeIn(2000);
+
+  // SMOOTH SCROLL
+  $(document).on('click', 'a[href^="#"]', function(e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
 });
 
-// SMOOTH SCROLL
-$(document).on('click', 'a[href^="#"]', function(e) {
-  e.preventDefault();
 
-  $('html, body').animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-  }, 500);
-});
+new WOW().init();
